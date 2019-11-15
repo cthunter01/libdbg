e9220896bea0711de959d441be1813ee3c34076f
Travis Hunter <travis.hunter@gmail.com>
2019-11-15 11:15:51 -0700

    Saving work in progress. Added CPURegisterEnum and Descriptor, as well as created a subfolder for writing test functions

--- Debugger.cpp
+++ Debugger.cpp
@@ -20,8 +20,91 @@
 Debugger::Debugger(const std::string& inProgName, const pid_t& inPid)
     : progName(inProgName),
       pid(inPid),
-      breakpointMap()
-{ }
+      breakpointMap(),
+      registerDescriptors()
+{
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r15,
+                           CPURegisterDescriptor(CPURegisterEnum::r15, 15, "r15")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r14,
+                           CPURegisterDescriptor(CPURegisterEnum::r14, 14, "r14")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r13,
+                           CPURegisterDescriptor(CPURegisterEnum::r13, 13, "r13")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r12,
+                           CPURegisterDescriptor(CPURegisterEnum::r12, 12, "r12")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rbp,
+                           CPURegisterDescriptor(CPURegisterEnum::rbp, 6, "rbp")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rbx,
+                           CPURegisterDescriptor(CPURegisterEnum::rbx, 3, "rbx")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r11,
+                           CPURegisterDescriptor(CPURegisterEnum::r11, 11, "r11")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r10,
+                           CPURegisterDescriptor(CPURegisterEnum::r10, 10, "r10")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r9,
+                           CPURegisterDescriptor(CPURegisterEnum::r9, 9, "r9")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::r8,
+                           CPURegisterDescriptor(CPURegisterEnum::r8, 8, "r8")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rax,
+                           CPURegisterDescriptor(CPURegisterEnum::rax, 0, "rax")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rcx,
+                           CPURegisterDescriptor(CPURegisterEnum::rcx, 2, "rcx")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rdx,
+                           CPURegisterDescriptor(CPURegisterEnum::rdx, 1, "rdx")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rsi,
+                           CPURegisterDescriptor(CPURegisterEnum::rsi, 4, "rsi")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rdi,
+                           CPURegisterDescriptor(CPURegisterEnum::rdi, 5, "rdi")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::orig_rax,
+                           CPURegisterDescriptor(CPURegisterEnum::orig_rax, -1, "orig_rax")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rip,
+                           CPURegisterDescriptor(CPURegisterEnum::rip, -1, "rip")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::cs,
+                           CPURegisterDescriptor(CPURegisterEnum::cs, 51, "cs")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::eflags,
+                           CPURegisterDescriptor(CPURegisterEnum::eflags, 49, "eflags")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::rsp,
+                           CPURegisterDescriptor(CPURegisterEnum::rsp, 7, "rsp")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::ss,
+                           CPURegisterDescriptor(CPURegisterEnum::ss, 52, "ss")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::fs_base,
+                           CPURegisterDescriptor(CPURegisterEnum::fs_base, 58, "fs_base")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::gs_base,
+                           CPURegisterDescriptor(CPURegisterEnum::gs_base, 59, "gs_base")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::ds,
+                           CPURegisterDescriptor(CPURegisterEnum::ds, 53, "ds")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::es,
+                           CPURegisterDescriptor(CPURegisterEnum::es, 50, "es")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::fs,
+                           CPURegisterDescriptor(CPURegisterEnum::fs, 54, "fs")));
+    registerDescriptors.insert(
+            std::make_pair(CPURegisterEnum::gs,
+                           CPURegisterDescriptor(CPURegisterEnum::gs, 55, "gs")));
+}
 
 void Debugger::run()
 {
@@ -124,6 +207,10 @@ int Debugger::handleCommand(const std::string& line)
         char* end;
         setBreakpointAt(std::strtol(addr.c_str(), &end, 16));
     }
+    if(is_prefix(command, "register"))
+    {
+
+    }
     else
     {
         std::cerr << "Unknown command" << std::endl;
@@ -139,3 +226,221 @@ void Debugger::setBreakpointAt(uint64_t address)
     bp.enable();
     breakpointMap[address] = bp;
 }
+
+uint64_t Debugger::getRegisterValue(const CPURegisterEnum::CPURegister reg)
+{
+    // See <sys/user.h> for the definition of this struct
+    user_regs_struct registers;
+    ptrace(PTRACE_GETREGS, pid, NULL, &registers);
+
+    switch(reg)
+    {
+        case CPURegisterEnum::r15:
+            return registers.r15;
+
+        case CPURegisterEnum::r14:
+            return registers.r14;
+            
+        case CPURegisterEnum::r13:
+            return registers.r13;
+            
+        case CPURegisterEnum::r12:
+            return registers.r12;
+            
+        case CPURegisterEnum::rbp:
+            return registers.rbp;
+            
+        case CPURegisterEnum::rbx:
+            return registers.rbx;
+            
+        case CPURegisterEnum::r11:
+            return registers.r11;
+            
+        case CPURegisterEnum::r10:
+            return registers.r10;
+            
+        case CPURegisterEnum::r9:
+            return registers.r9;
+            
+        case CPURegisterEnum::r8:
+            return registers.r8;
+            
+        case CPURegisterEnum::rax:
+            return registers.rax;
+            
+        case CPURegisterEnum::rcx:
+            return registers.rcx;
+            
+        case CPURegisterEnum::rdx:
+            return registers.rdx;
+            
+        case CPURegisterEnum::rsi:
+            return registers.rsi;
+            
+        case CPURegisterEnum::rdi:
+            return registers.rdi;
+            
+        case CPURegisterEnum::orig_rax:
+            return registers.orig_rax;
+            
+        case CPURegisterEnum::rip:
+            return registers.rip;
+            
+        case CPURegisterEnum::cs:
+            return registers.cs;
+            
+        case CPURegisterEnum::eflags:
+            return registers.eflags;
+            
+        case CPURegisterEnum::rsp:
+            return registers.rsp;
+            
+        case CPURegisterEnum::ss:
+            return registers.ss;
+            
+        case CPURegisterEnum::fs_base:
+            return registers.fs_base;
+            
+        case CPURegisterEnum::gs_base:
+            return registers.gs_base;
+            
+        case CPURegisterEnum::ds:
+            return registers.ds;
+            
+        case CPURegisterEnum::es:
+            return registers.es;
+            
+        case CPURegisterEnum::fs:
+            return registers.fs;
+            
+        case CPURegisterEnum::gs:
+            return registers.gs;
+
+        default:
+            return 0;
+    }
+}
+            
+void Debugger::getRegisterValue(const CPURegisterEnum::CPURegister reg, uint64_t value)
+{
+    // See <sys/user.h> for the definition of this struct
+    user_regs_struct registers;
+    ptrace(PTRACE_GETREGS, pid, NULL, &registers);
+
+    switch(reg)
+    {
+        case CPURegisterEnum::r15:
+            registers.r15 = value;
+            break;
+
+        case CPURegisterEnum::r14:
+            registers.r14 = value;
+            break;
+            
+        case CPURegisterEnum::r13:
+            registers.r13 = value;
+            break;
+            
+        case CPURegisterEnum::r12:
+            registers.r12 = value;
+            break;
+            
+        case CPURegisterEnum::rbp:
+            registers.rbp = value;
+            break;
+            
+        case CPURegisterEnum::rbx:
+            registers.rbx = value;
+            break;
+            
+        case CPURegisterEnum::r11:
+            registers.r11 = value;
+            break;
+            
+        case CPURegisterEnum::r10:
+            registers.r10 = value;
+            break;
+            
+        case CPURegisterEnum::r9:
+            registers.r9 = value;
+            break;
+            
+        case CPURegisterEnum::r8:
+            registers.r8 = value;
+            break;
+            
+        case CPURegisterEnum::rax:
+            registers.rax = value;
+            break;
+            
+        case CPURegisterEnum::rcx:
+            registers.rcx = value;
+            break;
+            
+        case CPURegisterEnum::rdx:
+            registers.rdx = value;
+            break;
+            
+        case CPURegisterEnum::rsi:
+            registers.rsi = value;
+            break;
+            
+        case CPURegisterEnum::rdi:
+            registers.rdi = value;
+            break;
+            
+        case CPURegisterEnum::orig_rax:
+            registers.orig_rax = value;
+            break;
+            
+        case CPURegisterEnum::rip:
+            registers.rip = value;
+            break;
+            
+        case CPURegisterEnum::cs:
+            registers.cs = value;
+            break;
+            
+        case CPURegisterEnum::eflags:
+            registers.eflags = value;
+            break;
+            
+        case CPURegisterEnum::rsp:
+            registers.rsp = value;
+            break;
+            
+        case CPURegisterEnum::ss:
+            registers.ss = value;
+            break;
+            
+        case CPURegisterEnum::fs_base:
+            registers.fs_base = value;
+            break;
+            
+        case CPURegisterEnum::gs_base:
+            registers.gs_base = value;
+            break;
+            
+        case CPURegisterEnum::ds:
+            registers.ds = value;
+            break;
+            
+        case CPURegisterEnum::es:
+            registers.es = value;
+            break;
+            
+        case CPURegisterEnum::fs:
+            registers.fs = value;
+            break;
+            
+        case CPURegisterEnum::gs:
+            registers.gs = value;
+            break;
+
+        default:
+            break;
+    }
+    ptrace(PTRACE_SETREGS, pid, NULL, &registers);
+}
+
+            
--- Debugger.h
+++ Debugger.h
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 
 #include "Breakpoint.h"
+#include "CPURegisterEnum.h"
+#include "CPURegisterDescriptor.h"
 
 using __gnu_cxx::hash_map;
 
@@ -19,10 +21,14 @@ public:
 
     typedef std::pair<std::string, std::string> CommandPairType;
 
+    uint64_t getRegisterValue(const CPURegisterEnum::CPURegister reg);
+    void setRegisterValue(const CPURegisterEnum::CPURegister reg, uint64_t value);
+
 private:
     std::string progName;
     pid_t pid;
     hash_map<uint64_t, Breakpoint> breakpointMap;
+    hash_map<CPURegisterEnum::CPURegister, CPURegisterDescriptor> registerDescriptors;
 };
 
 #endif // DEBUGGER_H
